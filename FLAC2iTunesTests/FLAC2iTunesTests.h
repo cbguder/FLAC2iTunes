903a137eafbf6298c8fdebe62560cadcdebf8a81
Can Berk Güder <cbguder@gmail.com>
2011-11-25 21:42:04 +0200

    Add unit tests.

---
+++ FLAC2iTunesTests/FLAC2iTunesTests.h
@@ -0,0 +1,13 @@
+//
+//  FLAC2iTunesTests.h
+//  FLAC2iTunesTests
+//
+//  Created by Can Berk Güder on 25/11/2011.
+//  Copyright (c) 2011 CBG. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FLAC2iTunesTests : SenTestCase
+
+@end
---
+++ FLAC2iTunesTests/FLAC2iTunesTests.m
@@ -0,0 +1,25 @@
+//
+//  FLAC2iTunesTests.m
+//  FLAC2iTunesTests
+//
+//  Created by Can Berk Güder on 25/11/2011.
+//  Copyright (c) 2011 CBG. All rights reserved.
+//
+
+#import "FLAC2iTunesTests.h"
+
+@implementation FLAC2iTunesTests
+
+- (void)setUp {
+	[super setUp];
+}
+
+- (void)tearDown {
+	[super tearDown];
+}
+
+- (void)testCapitalization {
+	STFail(@"Unit tests are not implemented yet in FLAC2iTunesTests");
+}
+
+@end
